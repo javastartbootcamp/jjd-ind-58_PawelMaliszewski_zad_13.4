fca75bca09205e7ddd22192c01cfdf5daac18331
Pawel Maliszewski <pablo999455@gmail.com>
2023-04-26 21:33:23 +0200

    solution

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -2,5 +2,71 @@ package pl.javastart.task;
 
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
-public class CustomList {
+public class CustomList<T> {
+
+    private static final int DEFAULT_CAPACITY = 100;
+    private int size = 0;
+    private final Object[] elements;
+
+    public CustomList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T) elements[index];
+    }
+
+    public void add(T t) {
+        if (size < DEFAULT_CAPACITY) {
+            elements[size] = t;
+            size++;
+        }
+    }
+
+    public void add(int index, T t) {
+        if (size < DEFAULT_CAPACITY) {
+            elements[size] = t;
+            size++;
+            moveIndex(index, t);
+        }
+    }
+
+    @SuppressWarnings("")
+    private void moveIndex(int index, T t) {
+        for (int i = size - 1; i > index; i--) {
+            T temp = (T) elements[i - 1];
+            elements[i - 1] = elements[i];
+            elements[i] = temp;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(int index) {
+        elements[index] = null;
+        moveRemovedIndex(index);
+        size--;
+    }
+
+    @SuppressWarnings("")
+    private void moveRemovedIndex(int index) {
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]).append(", ");
+        }
+        sb.replace(sb.length() - 2, sb.length(), "]");
+        return sb.toString();
+    }
 }
+
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -7,8 +7,18 @@ public class Main {
         // tutaj możesz przeprowadzać manualne testy listy
 
         // lista powinna być parametryzowana (analogicznie jak java.util.ArrayList czy java.util.LinkedList)
-        CustomList<String> customListOfStrings = new CustomList();
-        CustomList<Integer> customListOfIntegers = new CustomList();
+        CustomList<String> customListOfStrings = new CustomList<>();
+        CustomList<Integer> customListOfIntegers = new CustomList<>();
+
+        customListOfStrings.add("1");
+        customListOfStrings.add("2");
+        customListOfStrings.add("3");
+        customListOfStrings.add("4");
+        customListOfStrings.add("5");
+        System.out.println(customListOfStrings.toString());
+        customListOfStrings.add(2, "test");
+        customListOfStrings.remove(1);
+        System.out.println(customListOfStrings.toString());
 
     }
 }
